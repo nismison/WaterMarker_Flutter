d70347d9795f41b281e0b5aac16be446ad193d9b
nismison <nismison@gmail.com>
2025-11-18 18:51:28 +0800

    feat(app): 更新应用名称和包标识符

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.water_marker_test2"
+        applicationId = "com.watermarker.v2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
